18c498e9017b83601e1fef53967dcea68ce2ad7d
Cody Nguyen <36778689+CodyKN@users.noreply.github.com>
2020-12-02 22:42:12 -0600

    lofi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
+import { Switch, Link, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Lofi } from "./pages/lofi";
+
 export const App = () => {
   return (
-    <div>
-      Cody
-    </div>
+    <>
+      <div>
+        Cody's page
+      </div>
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/lofi">Lofi</Link>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/lofi" component={Lofi} />
+      </Switch>
+    </>
   );
 }
---
+++ src/pages/lofi/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Lofi = () => {
+    return (
+        <iframe
+            title='ChilledCow Lofi'
+            src="https://www.youtube.com/embed/5qap5aO4i9A" 
+            allow='autoplay; encrypted-media'
+            allowfullscreen
+            frameborder="0"
+            width="560" 
+            height="315" 
+        />
+    );
+}
